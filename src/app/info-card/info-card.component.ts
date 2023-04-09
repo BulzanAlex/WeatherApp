632a74f1f11615ad6ca3eed66027a4d10f468d18
Bulzan Alex <116515455+BulzanAlex@users.noreply.github.com>
2023-04-09 22:52:21 +0300

    Finished styling

--- src/app/info-card/info-card.component.ts
+++ src/app/info-card/info-card.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
-  selector: 'app-info-card',
+  selector: 'info-card',
   templateUrl: './info-card.component.html',
   styleUrls: ['./info-card.component.css']
 })
 export class InfoCardComponent {
-
+  @Input() cardName = 'string';
+  @Input() iconUrl = 'string';
+  @Input() cardValue = 'string';
 }
