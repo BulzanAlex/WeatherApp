0deb8ecb5f429f1ae4bec005b2743c8248fae38b
Bulzan Alex <116515455+BulzanAlex@users.noreply.github.com>
2023-04-10 16:40:11 +0300

    Defined API headers

--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -1,5 +1,6 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/enviroments/enviroment';
 
 @Injectable({
   providedIn: 'root'
@@ -8,8 +9,12 @@ export class WeatherService {
 
   constructor(private http: HttpClient) { }
 
-  getWeather() {
-    return this.http.get('')
+  getWeather(cityName: string) {
+    this.http.get(environment.weatherApiBaseUrl, {
+      headers: new HttpHeaders()
+      .set(environment.XRapidAPIHostHeaderName, environment.XRapidAPIHostHeaderValue)
+      .set(environment.XRapidAPIKeyHeaderName, environment.XRapidAPIKeyHeaderValue)
+    })
   }
 
 }
--- src/enviroments/enviroment.ts
+++ src/enviroments/enviroment.ts
@@ -1,3 +1,10 @@
 export const environment = {
-    production: false
+    production: false,
+    weatherApiBaseUrl: 'https://rapidweather.p.rapidapi.com/data/2.5/weather',
+
+    XRapidAPIHostHeaderName: 'X-RapidAPI-Host',
+    XRapidAPIHostHeaderValue: 'rapidweather.p.rapidapi.com',
+
+    XRapidAPIKeyHeaderName: 'X-RapidAPI-Key',
+    XRapidAPIKeyHeaderValue: '653be9c572mshdfebd2cfb7223d0p1047acjsne69b2e140c86',
 };
