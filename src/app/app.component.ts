41076a48bb9107a200d5ea903a2d7651e5325ace
Bulzan Alex <116515455+BulzanAlex@users.noreply.github.com>
2023-04-11 19:53:18 +0300

    got geolocation fed to weather api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { environment } from 'src/enviroments/enviroment';
 import { WeatherService } from './services/weather.service';
 import { WeatherData } from './models/weather.model';
+import { GeolocationService } from './services/geolocation.service';
+import { HttpClient } from '@angular/common/http';
+import { GeolocationData } from './models/geolocation.model';
 
 @Component({
   selector: 'app-root',
@@ -14,14 +17,21 @@ export class AppComponent implements OnInit {
   precipitation: number | string = 0;
   humidity: number | string = 0;
 
-  constructor(private weatherService: WeatherService) {
+  
+
+  constructor(private weatherService: WeatherService,
+    private http:HttpClient
+    ) {
 
   }
-  cityName: string = 'Lisbon';
+
+  cityNamee: string | undefined;
+  cityName: string = '';
   weatherData?: WeatherData;
+  geolocationData?: GeolocationData;
 
   ngOnInit(): void {
-    this.getWeatherData(this.cityName);
+    this.getGeolocationData();
     this.cityName = '';
   }
 
@@ -30,7 +40,7 @@ export class AppComponent implements OnInit {
     this.cityName = '';
   }
 
-  public getWeatherData(cityName: string) {
+  private getWeatherData(cityName: string) {
     this.weatherService.getWeatherData(cityName)
     .subscribe ({
       next: (response) => {
@@ -39,7 +49,27 @@ export class AppComponent implements OnInit {
         console.log(response);
         
         this.apiIconURL = this.weatherData.current.condition.icon;
+
+        
       }
+    });
+  }
+  geoApi = '0e85cc3431d6451890efc06c99dc9c03';
+  geoUrl = 'https://ipgeolocation.abstractapi.com/v1/?api_key=' + this.geoApi;
+
+  private getGeolocationData() {
+    this.http.get(this.geoUrl).subscribe((res:any)=>{
+
+      this.geolocationData = res;
+
+      this.cityNamee = this.geolocationData?.city.toString();
+
+      console.log(res);
+
+      if ( this.cityNamee){
+      this.getWeatherData(this.cityNamee);
+      };
+
     });
   }
 
--- src/app/services/geolocation.service.ts
+++ src/app/services/geolocation.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GeolocationService {
 
-  constructor() { }
+  geoApi = '0e85cc3431d6451890efc06c99dc9c03';
+
+  geoUrl = 'https://ipgeolocation.abstractapi.com/v1/?api_key=' + this.geoApi;
+
+  constructor(private http:HttpClient) { }
+
+
+
+  getGeolocationData() {
+    this.http.get(this.geoUrl).subscribe((res:any)=>{
+
+    });
+  };
+
 }
